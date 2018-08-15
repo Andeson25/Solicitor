3d788cfe0ae4b202796fb458d86d653a6c891c3f
kek <oleh.petryk@gmail.com>
2018-08-15 04:38:26 +0300

    commit

--- js/review.js
+++ js/review.js
@@ -1,1 +1,12 @@
 let show=false;
+function showFunc(){
+    if(!show){
+        show=true;
+        $('.content-integrity').removeClass('hideKey');
+        $('.content-integrity').addClass('showKey');
+    }else{
+        show=false;
+        $('.content-integrity').removeClass('showKey');
+        $('.content-integrity').addClass('hideKey');
+    }
+}
