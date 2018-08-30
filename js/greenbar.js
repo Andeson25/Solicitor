3a479ccfee68aa41113f94022cb12d1916acf7e7
kek <oleh.petryk@gmail.com>
2018-08-31 01:27:48 +0300

    made green bar

---
+++ js/greenbar.js
@@ -0,0 +1,40 @@
+data = [{
+    percent: "30"
+}, {
+    percent: "31"
+}, {
+    percent: "40"
+}];
+
+function setMarkers(data) {
+
+    let buttons = $('.float-block');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.left = data[i].percent + '%';
+        let int = parseInt(data[i].percent);
+        if (int <= 15) {
+            buttons[i].firstElementChild.style.color = "rgb(34, 177, 76)";
+        } else if (int <= 39) {
+            buttons[i].firstElementChild.style.color = 'rgb(172, 220, 22)';
+        } else if (int <= 58) {
+            buttons[i].firstElementChild.style.color = 'rgb(172, 220, 22)';
+        } else if (int <= 75) {
+            buttons[i].firstElementChild.style.color = 'rgb(253, 190, 63)';
+        } else if (int <= 100) {
+            buttons[i].firstElementChild.style.color = 'rgb(237, 28, 36)';
+        }
+        if (
+            buttons[i - 1] !== undefined && parseFloat(buttons[i].style.left.split('%')[0]) - parseFloat(buttons[i - 1].style.left.split('%')[0]) <= 20
+            ||
+            buttons[i + 1] !== undefined && parseFloat(buttons[i + 1].style.left.split('%')[0]) - parseFloat(buttons[i].style.left.split('%')[0]) <= 20
+        ) {
+            if (buttons[i - 1] !== undefined && buttons[i - 1].classList.contains('float-bottom')) {
+
+            } else {
+                buttons[i].classList.add('float-bottom');
+            }
+        }
+    }
+}
+setMarkers(data)
